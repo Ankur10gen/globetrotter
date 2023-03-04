779f3ab6bb2105da2260fdde02a25511c4691841
Ankur Raina <Ankur10gen@users.noreply.github.com>
2023-03-05 02:10:27 +0800

    fixed app id

--- gtmanager/ui/gtui/components/useApp.js
+++ gtmanager/ui/gtui/components/useApp.js
@@ -5,7 +5,7 @@ export function useApp() {
   const [app, setApp] = useState(null);
   // Run in useEffect so that App is not created in server-side environment
   useEffect(() => {
-    setApp(Realm.getApp(process.env.NEXT_PUBLIC_APP_ID));
+    setApp(Realm.getApp("gtmgrbackend-qymjf"));
   }, []);
   return app;
 }
